5ffd1c44701d0866dbfffe6b2f61d25b602c47b0
Machiavelli <makaveli643@gmail.com>
2021-07-25 12:40:20 +0100

    OCT

--- ALGO/conv.py
+++ ALGO/conv.py
@@ -10,5 +10,32 @@ def Bin(num:int):
         num //= 2
     return RevString(string)
 
+#to convert from binary to  decimal
+def Dec(string:str):
+    summ = 0
+    i = len(string)
+    j = 0
+    while (i != 0):
+        i -=1
+        summ += int(string[j])* (2**i)
+        j +=1
+    return summ
 
+
+
+
+#to convert octal to binary
+def OctBin(num:int):
+    res = ""
+    s = str(num)
+    b = ""
+    for i in range(len(s)):
+        b = Bin(int(s[i]))
+        if len(b) < 3 :
+            for j in range (3-len(b)): b = "0"+b
+        res += b
+    return res[1: len(res)]
+        
+print(Dec("1001011000"))
 print(Bin(600))
+print(OctBin(35))
