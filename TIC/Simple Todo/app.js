d00c09670608d31bb79c0284c9dc6659c11889a6
onlymachiavelli <tmakaveli643@gmail.com>
2021-08-09 18:04:30 +0100

    TST

--- TIC/Simple Todo/app.js
+++ TIC/Simple Todo/app.js
@@ -1,14 +1,12 @@
-
 //make js easier!
-const Class = (cl, ind) => document.getElementsByClassName(cl)[ind]
-const Id = id => document.getElementsByClassName(id)
+const Class = (cl, ind) => document.getElementsByClassName(cl)[ind];
+const Id = (id) => document.getElementsByClassName(id);
 
-//Use Localstorage 
-const Get = Key => localStorage.getItem(Key)
-const Add = (Key, Val) => localStorage.setItem(Key, Val)
-const Clear = () => localStorage.clear()
-const Delete = Key => localStorage.removeItem(Key)
-
+//Use Localstorage
+const Get = (Key) => localStorage.getItem(Key);
+const Add = (Key, Val) => localStorage.setItem(Key, Val);
+const Clear = () => localStorage.clear();
+const Delete = (Key) => localStorage.removeItem(Key);
 
 /*
 Schema : 
@@ -16,4 +14,4 @@ title: str
 desc : str 
 id : int
 
-*/ 
+*/
