4f52aef85ccc0997311bbb6ab5c72c74619557eb
onlymachiavelli <tmakaveli643@gmail.com>
2021-09-23 18:48:30 +0100

    Fck ;

--- TIC/Simple Todo/app.js
+++ TIC/Simple Todo/app.js
@@ -1,12 +1,13 @@
 //make js easier!
-const Class = (cl, ind) => document.getElementsByClassName(cl)[ind];
-const Id = (id) => document.getElementsByClassName(id);
+const Class = (cl, ind) => document.getElementsByClassName(cl)[ind]
+const Id = (id) => document.getElementsByClassName(id)
 
 //Use Localstorage
-const Get = (Key) => localStorage.getItem(Key);
-const Add = (Key, Val) => localStorage.setItem(Key, Val);
-const Clear = () => localStorage.clear();
-const Delete = (Key) => localStorage.removeItem(Key);
+const Get = (Key) => localStorage.getItem(Key)
+const Add = (Key, Val) => localStorage.setItem(Key, Val)
+const Clear = () => localStorage.clear()
+const Delete = (Key) => localStorage.removeItem(Key)
+//if()
 
-let Container = Class("container", 0);
-Class("addbtn", 0).addEventListener("click", () => {});
+let Container = Class("container", 0)
+Class("addbtn", 0).addEventListener("click", () => {})
