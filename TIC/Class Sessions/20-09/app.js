4d5effd02a85ef78adab029ee9c27c6ad5d9a7c9
onlymachiavelli <tmakaveli643@gmail.com>
2021-09-26 12:03:15 +0100

    Log

--- TIC/Class Sessions/20-09/app.js
+++ TIC/Class Sessions/20-09/app.js
@@ -1,8 +1,2 @@
 const Class = (cl, ind) => document.getElementsByClassName(cl)[ind]
 const Id = (id) => document.getElementById(id)
-//Initial FRAME SOURCE
-Class("comp", 0).src = "welc.html"
-
-Class("link", 0).addEventListener("click", () => {
-  alert("tst")
-})
