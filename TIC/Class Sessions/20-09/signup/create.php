194df4212a02f847ae0372984df70ec14e53d4a8
onlymachiavelli <tmakaveli643@gmail.com>
2021-09-27 21:46:59 +0100

    FindTarget function

--- TIC/Class Sessions/20-09/signup/create.php
+++ TIC/Class Sessions/20-09/signup/create.php
@@ -5,8 +5,28 @@ $password = "";
 
 $connection = new mysqli($hostname, $username, $password, "eleve");
 
+
+function findTarget($target){
+    global $connection;
+    $action = "SELECT * FROM users";
+    $res = mysqli_query($connection, $action);
+    while ($row = $res -> fetch_assoc()){
+        if($row['cin'] == $target) {
+            return TRUE;
+        }
+    }
+    return FALSE;
+}
+
+
 if($connection) {
-    print("Connected !");
+    
+    if(findTarget("01447896325")) {
+        print("it's here");
+    }
+    else {
+        print("it does not exist !");
+    }
 }
 else {
     print("Error conecting to DB");
