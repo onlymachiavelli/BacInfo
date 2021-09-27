38ce2e461c183870dd0a53bf0fe90852b52ad23e
onlymachiavelli <tmakaveli643@gmail.com>
2021-09-28 00:04:09 +0100

    PATH

--- TIC/Class Sessions/20-09/Login/auth.php
+++ TIC/Class Sessions/20-09/Login/auth.php
@@ -1,4 +1,20 @@
 <?php
+$servername = "localhost";
+$userame = "root";
+$password = "";
+$connection = new mysqli($servername, $userame, $password, "eleve");
 
+function makeAuth($username, $password){
+    return 0;
+}
+
+
+if ($connection) {
+    print("Connected to DB");
+
+}
+else {
+    die ("Error Connecting to DB");
+}
 
 ?>
