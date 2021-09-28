3cad110b51f2e9ae2e052975744eb67ab97bcf31
onlymachiavelli <tmakaveli643@gmail.com>
2021-09-28 20:43:45 +0100

    Connecting to db (profile)

--- TIC/Class Sessions/20-09/profile/profile.php
+++ TIC/Class Sessions/20-09/profile/profile.php
@@ -2,8 +2,35 @@
 session_start();
 if (!isset($_SESSION['user']) && !isset($_SESSION['pass'])) header('Location: ../login/login.php');
 
+
+
 else {
-    print("AUTHED !");
-    echo ($_SESSION['user']);
+    $hostname = "localhost";
+    $username = "root";
+    $password = "";
+    $connection = new mysqli($hostname, $username, $password, "eleve");
+
+    if($connection){
+        print("CONNECTED !");
+
+    }
+    else print("ERR CONNECTING TO DB !");
+}
+
+
+function getTarget($cin){
+    global $connection;
 }
 ?>
+
+
+
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <link rel="stylesheet" href="" />
+        <?php echo("<title>".getTarget($_SESSION['cin'])."</title>") ?>
+    </head>
+    <body></body>
+</html>
