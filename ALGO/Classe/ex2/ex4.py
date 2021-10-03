334643329059828af07710999382d4b7c9e9aa61
onlymachiavelli <tmakaveli643@gmail.com>
2021-10-03 18:29:33 +0100

    Algo

--- ALGO/Classe/ex2/ex4.py
+++ ALGO/Classe/ex2/ex4.py
@@ -1,10 +1,34 @@
 def word():
     while(1):
         word = input("ENTER THE WORD ")
-        if(word.isupper() and word.isalpha()):
+        if(word.isupper() and word.isalpha() and len(word) in range(3, 11)):
             return word
 
 
-def fillMat(word, mat: list):
-    actuallist = []
+def find(string: str, target: str):
+    for i in range(len(string)):
+        if string[i] == target:
+            return True
+    return None
+
+
+def fillMat(word: str, mat: list):
     apl = "ABCDEFGHIJKLMNOPQRSTUVXYZ"
+
+    counter = 0
+    for i in range(5):
+        empty = []
+        for j in range(5):
+            empty.append(apl[counter])
+            counter += 1
+        mat.append(empty)
+
+
+mymat = []
+
+myword = word()
+
+
+print(mymat)
+fillMat(myword, mymat)
+print(mymat)
