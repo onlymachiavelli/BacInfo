f08213fe0dbad2abf83c14439d97c495033d1783
onlymachiavelli <tmakaveli643@gmail.com>
2021-10-06 20:03:18 +0100

    Counting

--- TIC/Class Sessions/20-09/welc.php
+++ TIC/Class Sessions/20-09/welc.php
@@ -4,37 +4,38 @@
     $username = "root";
     $password = "";
     $connection = new mysqli($hostname, $username, $password, "eleve");
-
+    function startsWith($string, $ele){
+        for ($i =0;$i<count($ele);$i++) if( $string[$i] != $ele[$i]) return FALSE;
+        return TRUE;
+    }
     if($connection){
-        print("Connected");
-
+        $classes = [
+            1 => 0,
+            2=>0,
+            3=>0,
+            4=>0
+        ];
+        $action = "SELECT * FROM users";
+        $res = $connection -> query($action);
+    
+        while ($row = $res -> fetch_assoc()){
+            if(startsWith(strtolower($row['class']), "1ere")) $classes[1]++;
+            else if(startsWith(strtolower($row['class']), "2eme")) $classes[2]++;
+            else if(startsWith(strtolower($row['class']), "3eme")) $classes[3]++;
+            else $classes[4]++;
+    
+        }
     }
     else {
         die ("error connecting to daya nase ");
     }
 
 
-    function startsWith($string, $ele){
-        for ($i =0;$i<count($ele);$i++) if( $string[$i] != $ele[$i]) return FALSE;
-        return TRUE;
-    }
-    $classes = [
-        1 => 0,
-        2=>0,
-        3=>0,
-        4=>0
-    ];
-    $action = "SELECT * FROM users";
-    $res = $connection -> query($action);
+    
+    
+   
+    
 
-    while ($row = $res -> fetch_assoc()){
-        if(startsWith(strtolower($row['class']), "1ere")) $classes[1]++;
-        else if(startsWith(strtolower($row['class']), "2eme")) $classes[2]++;
-        else if(startsWith(strtolower($row['class']), "3eme")) $classes[3]++;
-        else $classes[4]++;
-
-    }
-    print($classes);
 ?>
 
 <!DOCTYPE html>
@@ -62,7 +63,42 @@
                 Classe : 
             </td>
             <td>
-                4eme Science Informatique
+                Nombre
+            </td>
+        </tr>
+        <tr>
+            <td>1Ere</td>
+            <td>
+                <?php
+                    echo($classes[1]);
+                ?>
+            </td>
+        </tr>
+
+        <tr>
+            <td>2Eme</td>
+            <td>
+                <?php
+                    echo($classes[2]);
+                ?>
+            </td>
+        </tr>
+
+        <tr>
+            <td>3Eme</td>
+            <td>
+                <?php
+                    echo($classes[3]);
+                ?>
+            </td>
+        </tr>
+
+        <tr>
+            <td>4Eme</td>
+            <td>
+                <?php
+                    echo($classes[4]);
+                ?>
             </td>
         </tr>
     </table>
