e79bdf40ba4102dc7115a84e3ff26e45f5fa1d11
onlymachiavelli <tmakaveli643@gmail.com>
2021-10-12 16:39:32 +0100

    Ex

--- ALGO/Classe/Files/ex1.py
+++ ALGO/Classe/Files/ex1.py
@@ -1,5 +1,16 @@
 def size():
     while(1):
-        n = input("Enter the size")
+        n = int(input("Enter the size"))
         if n > 2:
             break
+
+
+def fillFile(n):
+    myFile = open("data.txt", "w")
+    for i in range(n):
+        myFile.write(input("Enter DATA")+"\n")
+    myFile.close()
+
+
+n = size()
+fillFile(n)
