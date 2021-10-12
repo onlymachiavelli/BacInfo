14a1691bda6d92027a20cda3a5ec527d35e1f981
onlymachiavelli <tmakaveli643@gmail.com>
2021-10-12 18:24:44 +0100

    Ex

---
+++ ALGO/Classe/Files/ex2.py
@@ -0,0 +1,11 @@
+def Size():
+    while(1):
+        n = int(input("Enter the size "))
+        if n in range(2, 16):
+            return n
+
+def fillMath(mat:list, size:int):
+    line = []
+    for i in range(n):
+        for j in range(n):
+            entry = input("Enteer thee caracter ")
