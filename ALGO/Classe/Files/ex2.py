378ad43c6f74140c4bacdce10b315cfdb8be002f
onlymachiavelli <tmakaveli643@gmail.com>
2021-10-22 22:17:37 +0100

    Ex

--- ALGO/Classe/Files/ex2.py
+++ ALGO/Classe/Files/ex2.py
@@ -27,5 +27,40 @@ debut
     retourner n
 fin 
 
-tdol
+TDOL : 
+    n : entier
+
+
+procedure fillMat(n:entier, @mat:matrice)
+debut 
+
+    pour i de 0 a n-1 faire
+        pour j de 0 a n-1 faire
+            repeter 
+                ecrire("Enter",i,j)
+                lire(mat[i,j])
+            jusqua "A" <=mat[i,j] <= "Z"
+        fin pour
+    fin pour
+
+fin
+TDOL:
+    i : entier
+    j : entier
+
+
+
+procedure fillFile(n:entier, m:matrice, @f)
+debut 
+    ouvrir("c:\bac22\sym.txt", f, "w")
+    nbc = =
+    nbl = 0
+    chl = "*"
+    chc = "*"
+    pour i de 0 a n-1 faire 
+        ch1 = ""
+        ch2  = ""
+        pour j de 0 a n-1 faire 
+            
+fin
 """
