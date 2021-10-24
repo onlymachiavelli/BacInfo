426ca61eb2fc76c7d5bb43cecfb2ca18ce8bb354
Mahciavelli <tmakaveli643@gmail.com>
2021-10-24 14:55:47 +0100

    ex

--- TIC/Class Sessions/Verf inputs/app.js
+++ TIC/Class Sessions/Verf inputs/app.js
@@ -8,7 +8,7 @@ const Id = id => document.getElementById(id)
 
 const Verf = () =>{
     res = ``
-    if(Id("name").value.length > 0) res += "Good Name \n" 
+    if(Id("name").value.length > 0 && Id("name").value.indexOf(" ") !== -1) res += "Good Name \n" 
     else res += "Invalid name \n"
     if (Id("cin").value.length === 8 && (Id("cin").value.charAt(0) === "0" || Id("cin").value.charAt(0) ==="1") && !isNaN(Id("cin").value)) res += "Good CIN input"
     else res += "invalid cin input"
