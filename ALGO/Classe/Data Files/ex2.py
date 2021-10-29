1699380b20c4e2fc0ce098b5009ef988c403ce2e
onlymachiavelli <tmakaveli643@gmail.com>
2021-10-29 09:37:27 +0100

    EX

---
+++ ALGO/Classe/Data Files/ex2.py
@@ -0,0 +1,30 @@
+from pickle import *
+
+def remplirfc(n):
+    
+    myfile = open("commande.dat", "wb")
+    n = 0
+    quit = False
+        while(not quit):
+            
+            name = input("enter name ")
+            qte = int(input("Enter qte "))
+            price = float(input("Enter price "))
+            state = bool(input("Enter 1 : True or 0 : False"))
+            
+            dump(
+                {
+                    "name":name,
+                    "qte":qte,
+                    "price":price,
+                    "state":state,
+                    },
+                myFile
+                )
+            n += 1
+            print("Would you like to keep going ? Y if you want to keep or something else if you want to stop ")
+            ans = input().upper()
+            if (answer !=  "Y") : quit = True
+        
+            
+            
