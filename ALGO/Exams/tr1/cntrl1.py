78a477b5a0a250be73713a48f00c6598d4ee603d
onlymachiavelli <tmakaveli643@gmail.com>
2021-11-14 11:27:42 +0100

    ec

--- ALGO/Exams/tr1/cntrl1.py
+++ ALGO/Exams/tr1/cntrl1.py
@@ -1,6 +1,6 @@
 
-
-from os import readlink
+from pickle import *
+import pickle
 
 
 def Exist(ele: str):
@@ -16,6 +16,7 @@ def fillMVT():
     quit = False
 
     while not quit:
+        n += 1
         q = False
         while not q:
             num = input("enter Number ")
@@ -29,4 +30,25 @@ def fillMVT():
             year = input("Enter year ")
 
             q = len(day) == 2 and day.isnumeric() and len(
-                month) == 2 and month.isnumeric() and len(year) == 4
+                month) == 2 and month.isnumeric() and len(year) == 4 and year.isnumeric()
+        date = day+"/"+month+"/"+year
+
+        q = False
+        while not q:
+            mont = float(input("Enter mont "))
+            q = mont > 0
+        q = False
+        while not q:
+            nat = input("Enter nat ")
+            q = nat == "D" or nat == "C"
+
+        pickle.dump(fv, {
+            "num": num,
+            "date": date,
+            "mont": mont,
+            "nat": nat
+        })
+
+        print("Do you ant to add anther one ?  y/anything else")
+        ans = input()
+        quit = ans.upper() == "Y" or "YES"
