4bcae133a917d85077ec9a084d35f31752731873
onlymachiavelli <tmakaveli643@gmail.com>
2021-11-14 11:15:46 +0100

    ec

--- ALGO/Exams/tr1/cntrl1.py
+++ ALGO/Exams/tr1/cntrl1.py
@@ -1,4 +1,32 @@
+
+
+from os import readlink
+
+
+def Exist(ele: str):
+    myFile = open("compte.txt", "r")
+    Lines = myFile.readlines()
+    return ele in Lines
+
+
 def fillMVT():
     global n
     fc = open("compte.txt", "r")
     fv = open("mvt.dat", "wb")
+    quit = False
+
+    while not quit:
+        q = False
+        while not q:
+            num = input("enter Number ")
+
+            q = Exist(num)
+
+        q = False
+        while not q:
+            day = input("Enter the day ")
+            month = input("Enter month ")
+            year = input("Enter year ")
+
+            q = len(day) == 2 and day.isnumeric() and len(
+                month) == 2 and month.isnumeric() and len(year) == 4
