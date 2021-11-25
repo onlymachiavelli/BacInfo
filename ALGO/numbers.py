09dc72565ce7b3984f1fb5b01902b11242dfe1d3
onlymachiavelli <tmakaveli643@gmail.com>
2021-11-25 23:18:38 +0100

    ex

--- ALGO/numbers.py
+++ ALGO/numbers.py
@@ -1,11 +1,27 @@
 
 
 def isPrime(n, i=2):
-    if n // 2 > n:
-        return False
-    if n % i == 0:
+    if n // 2 > i:
         return True
+    if n < 2 or n % i == 0:
+        return False
     return isPrime(n, i + 1)
 
 
-print(isPrime(4))
+#print([isPrime(x) for x in [1, 9, 20]])
+
+
+def tri(n, t):
+    change = True
+    while change:
+        change = False
+        for i in range(n-1):
+            if t[i] > t[i+1]:
+                t[i], t[i+1] = t[i+1], t[i]
+                change = True
+
+
+t = [0, 8, 6, 7, 4, 3]
+n = len(t)
+tri(n, t)
+print(t)
