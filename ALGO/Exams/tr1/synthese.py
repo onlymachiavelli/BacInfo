7db8532d79e2fdbe58e80a3fbbf501d6727f492f
onlymachiavelli <tmakaveli643@gmail.com>
2021-12-06 15:00:10 +0100

    exm

--- ALGO/Exams/tr1/synthese.py
+++ ALGO/Exams/tr1/synthese.py
@@ -6,6 +6,24 @@ import numpy as np
 l = r.randint(2, 10)
 c = r.randint(2, 10)
 
+
+def exist(l, c, m, n):
+    for i in range(c):
+        if n == m[l][i]:
+            return True
+
+    return False
+
+
+def fillMat(l, c, m):
+    for i in range(l):
+        for j in range(c):
+            while True:
+
+                if(not exist()):
+                    break
+
+
 m = np.zeros([l, c], dtype=int)
 
 print(m)
