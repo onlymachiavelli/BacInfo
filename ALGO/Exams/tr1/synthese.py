f574d573ad123c500bb098346cb77fc780f29360
onlymachiavelli <tmakaveli643@gmail.com>
2021-12-15 22:03:43 +0100

    ex

--- ALGO/Exams/tr1/synthese.py
+++ ALGO/Exams/tr1/synthese.py
@@ -1,55 +1,18 @@
-import random as r
-
-import numpy as np
+import random as r 
 from pickle import *
+import numpy as np 
 
 
-def exist(l, c, m, n):
-    for i in range(c):
-        if n == m[l][i]:
-            return True
-    return False
+def fillMat (m:np.ndarray,l:int, c:int ):
+    for i in range (l):
+        for j in range (c):
+            num = r.randint(-99, 100)
+            
 
+values = input("Enter the lengths ").split()
+l, c = map(int , values)
 
-def SelectionSort(n, t):
-    for i in range(n-1):
-        j = i
-        tmp = t[i]
-        while j >= 1 and tmp.n < t[j]
+mat = np.zeros([l,c], dtype=int)
+print(mat)
 
-
-def fillMat(l, c, m):
-    for i in range(l):
-        for j in range(c):
-            while True:
-                num = r.randint(-99, 100)
-                if(not exist(i, j, m, num)):
-                    break
-            m[i][j] = num
-
-
-def fillFile(l, c, m):
-    myFile = open("file.dat", "w")
-    for i in range(l):
-        s1 = 0
-        s2 = 0
-        su = False
-        j = 0
-        while j != c-1 or not su:
-            s2 = 0
-            j += 1
-            s1 += m[i][j]
-
-            for k in range(j+1, c):
-                s2 += m[i][k]
-            su = s1 == s2
-        print(su)
-
-
-l = r.randint(2, 10)
-c = r.randint(2, 10)
-
-m = np.zeros([l, c], dtype=int)
-fillMat(l, c, m)
-fillFile(l, c, m)
-print(m)
+print(type(mat))
