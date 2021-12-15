55c25a2fdc8020f9ec9dab628cc9b13a92547c02
onlymachiavelli <tmakaveli643@gmail.com>
2021-12-15 22:13:06 +0100

    ex

--- ALGO/Exams/tr1/synthese.py
+++ ALGO/Exams/tr1/synthese.py
@@ -2,12 +2,19 @@ import random as r
 from pickle import *
 import numpy as np 
 
-
+def exist(m:np.ndarray, l:int, c:int, num:int):
+    if (c > 0 ):
+        for i in range (c):
+            if m[l][i] == num: return True
+    return False
 def fillMat (m:np.ndarray,l:int, c:int ):
     for i in range (l):
         for j in range (c):
-            num = r.randint(-99, 100)
-            
+            while True:
+                num = r.randint(-99, 100)
+                if (not exist (m, i,j, num)):
+                    break 
+                m[i][j] = num
 
 values = input("Enter the lengths ").split()
 l, c = map(int , values)
