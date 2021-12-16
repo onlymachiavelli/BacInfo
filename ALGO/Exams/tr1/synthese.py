92555e0511639fcbb47faa3f3cee784c32728574
onlymachiavelli <tmakaveli643@gmail.com>
2021-12-16 14:14:41 +0100

    userCheck

--- ALGO/Exams/tr1/synthese.py
+++ ALGO/Exams/tr1/synthese.py
@@ -1,25 +1,38 @@
-import random as r 
+import random as r
 from pickle import *
-import numpy as np 
+import numpy as np
 
-def exist(m:np.ndarray, l:int, c:int, num:int):
-    if (c > 0 ):
-        for i in range (c):
-            if m[l][i] == num: return True
+
+def userCheck(l: int, c: int):
+    if (l not in range(3, 11) and c not in range(3, 11)):
+        return False
+    return True
+
+
+def exist(m: np.ndarray, l: int, c: int, num: int):
+    if (c > 0):
+        for i in range(c):
+            if m[l][i] == num:
+                return True
     return False
-def fillMat (m:np.ndarray,l:int, c:int ):
-    for i in range (l):
-        for j in range (c):
+
+
+def fillMat(m: np.ndarray, l: int, c: int):
+    for i in range(l):
+        for j in range(c):
             while True:
                 num = r.randint(-99, 100)
-                if (not exist (m, i,j, num)):
-                    break 
+                if (not exist(m, i, j, num)):
+                    break
                 m[i][j] = num
 
-values = input("Enter the lengths ").split()
-l, c = map(int , values)
 
-mat = np.zeros([l,c], dtype=int)
+values = input("Enter the lengths ").split()
+while True:
+    l, c = map(int, values)
+    if (userCheck(l, c)):
+        break
+mat = np.zeros([l, c], dtype=int)
 print(mat)
 
 print(type(mat))
