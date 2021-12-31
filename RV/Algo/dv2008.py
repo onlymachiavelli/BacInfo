a56f62a7c32710ea4331141e2213236247a80b44
onlymachiavelli <tmakaveli643@gmail.com>
2021-12-31 01:58:34 +0100

    filling up ex

--- RV/Algo/dv2008.py
+++ RV/Algo/dv2008.py
@@ -1,14 +1,9 @@
-print("hello world")
+import numpy as np
+import random as r
 
 
-def f(x: float, n: int, t: list):
-    i = 0
-    while x != t[i] and i < n:
-        i += 1
-        print(i)
-    return x == t[i]
-
-
-t = [2.5, 3, 100, 99, 5, 3.7, 0, 5]
-
-print(f(2, len(t), t))
+def fillMat(mat):
+    alpha = "abcdefghijklmnopqrstuvwxyz"
+    for i in range(6):
+        for j in range(6):
+            mat[i][j] = alpha[r.choice(alpha)[0]]
