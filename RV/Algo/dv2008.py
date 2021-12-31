db0bcc2648a0937a3cc048292afa1cffd38befb2
onlymachiavelli <tmakaveli643@gmail.com>
2021-12-31 02:13:45 +0100

    missing something

--- RV/Algo/dv2008.py
+++ RV/Algo/dv2008.py
@@ -2,11 +2,18 @@ import numpy as np
 import random as r
 
 
+def receiveTxt():
+    while True:
+        text = input("Enter the text ")
+        if 0 < len(text) <= 18:
+            return text
+
+
 def fillMat(mat):
     alpha = "abcdefghijklmnopqrstuvwxyz"
     for i in range(6):
         for j in range(6):
-            mat[i][j] = alpha[r.choice(alpha)[0]]
+            mat[i][j] = r.choice(alpha)[0]
 
 
 def cryptMsg(text: str, mat):
@@ -22,3 +29,16 @@ def cryptMsg(text: str, mat):
                 if text[i] == " ":
                     secret += " "
     return secret
+
+
+def fillS_cnd_Mat(secret: str, mat, key: str):
+    return 0
+
+
+mat = np.array([[""]*6]*6, dtype="str")
+matCrypt = np.array([[""]*6]*6, dtype="str")
+
+
+fillMat(mat)
+text = receiveTxt()
+print(cryptMsg(text, mat))
