ad703e2e6ff51b079e712cc0175a354213acf77a
onlymachiavelli <tmakaveli643@gmail.com>
2021-12-31 02:03:43 +0100

    returning func secret crypt shiit

--- RV/Algo/dv2008.py
+++ RV/Algo/dv2008.py
@@ -7,3 +7,18 @@ def fillMat(mat):
     for i in range(6):
         for j in range(6):
             mat[i][j] = alpha[r.choice(alpha)[0]]
+
+
+def cryptMsg(text: str, mat):
+    secret = ""
+    x = "ABCDEF"
+    ind = 0
+    while (ind != len(text)):
+        ind += 1
+        for i in range(6):
+            for j in range(6):
+                if text[ind] == mat[i][j]:
+                    secret += x[i] + x[j]
+                if text[i] == " ":
+                    secret += " "
+    return secret
