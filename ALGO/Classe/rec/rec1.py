4fa2d4ba5f886453ea367c42a4070a155a36975d
onlymachiavelli <tmakaveli643@gmail.com>
2022-01-04 17:42:48 +0100

    ex

--- ALGO/Classe/rec/rec1.py
+++ ALGO/Classe/rec/rec1.py
@@ -1,9 +1,8 @@
-def rec(n:int):
-    if n ==0 :
-        return 1
-    else :
-        return n * rec(n-1)
+def rec(myString:str):
+    for i in range (len(myString) //2) :
+        if myString[i] != myString[len(myString) -1 -i] :
+            return False 
+    return True 
 
 
 
-print(rec(10))
