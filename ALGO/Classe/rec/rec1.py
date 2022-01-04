01723557f4453beb7cbe8cd912092ea7987784b2
onlymachiavelli <tmakaveli643@gmail.com>
2022-01-04 20:26:47 +0100

    ex algo

--- ALGO/Classe/rec/rec1.py
+++ ALGO/Classe/rec/rec1.py
@@ -4,5 +4,18 @@ def rec(myString:str):
             return False 
     return True 
 
+def recString(myString:str,l:int, b:int, e:int) :
+    if ( b == e or b+1 == e):
+        return True
+    else:
+        if myString[b] == myString[e] :
+            return True and recString(myString,l,b+1,e-1)
+        else:
+            return False
+    
+            
 
 
+a = "liil"
+l = len(a)
+print(recString(a,l-1,0,l-1))
