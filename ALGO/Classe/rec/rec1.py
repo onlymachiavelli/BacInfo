a8cdaf8aa62bbf790e648758fe1a6119cdd44e62
onlymachiavelli <tmakaveli643@gmail.com>
2022-01-10 09:45:44 +0100

    ex

--- ALGO/Classe/rec/rec1.py
+++ ALGO/Classe/rec/rec1.py
@@ -1,11 +1,7 @@
-def repeat (arr:list, x:int,n:int, i:int=0):
-    if i == n :
-        return 0
-    else : 
-        if arr[i] == x :
-            return 1 + repeat(arr, x, i+1)
-        else :
-            return repeat(arr, x, i+1 )
-arr = [1,1,3,5,6,8,1]
 
-print(repeat(arr, 1,len(arr) ))
+
+
+def checkN(n:int):
+    return 40000 < n < 100000
+
+def isPrime(n:int):
