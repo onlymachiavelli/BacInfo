1379b7ad16ef4687278bc876d1fd52f08d81304a
onlymachiavelli <tmakaveli643@gmail.com>
2022-01-12 22:39:25 +0100

    ex

--- ALGO/Classe/rec/rec1.py
+++ ALGO/Classe/rec/rec1.py
@@ -1,12 +1,44 @@
+
+import pickle
+from math import sqrt
+
+
 def size():
 
     n = int(input("Enter n "))
-    quit = 40000 < n < 100000
-    if quit:
+    if 40000 < n < 100000:
         return n
-    else:
-        return size()
+    return size()
 
 
 def isPrime(n: int, i: int = 2):
-    return 0
+    if i > sqrt(n):
+        return True
+    return n % i != 0 and isPrime(n, i + 1)
+
+
+def mainFunc(n: int):
+    myFile = open("theResult.dat", "wb")
+    for i in range(1, n):
+        a = i
+        counter = [0, 1]
+        while a > 1:
+            if isPrime(i):
+                counter[0] += 1
+                if isPrime(a):
+                    counter[1] += 1
+                res = {
+                    "Num": a,
+                    "isSuperPrime": counter[0] == counter[1],
+
+                }
+                myFile.dump(
+                    res, myFile
+                )
+                print(res)
+                a //= 10
+    myFile.close()
+
+
+n = size()
+mainFunc(n)
