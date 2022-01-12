4aef305d4efd56d5445473681b022cd82755f0e7
onlymachiavelli <tmakaveli643@gmail.com>
2022-01-12 22:22:11 +0100

    ex

--- ALGO/Classe/rec/rec1.py
+++ ALGO/Classe/rec/rec1.py
@@ -1,7 +1,12 @@
+def size():
+
+    n = int(input("Enter n "))
+    quit = 40000 < n < 100000
+    if quit:
+        return n
+    else:
+        return size()
 
 
-
-def checkN(n:int):
-    return 40000 < n < 100000
-
-def isPrime(n:int):
+def isPrime(n: int, i: int = 2):
+    return 0
