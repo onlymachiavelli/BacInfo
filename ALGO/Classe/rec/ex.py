806ebf319203cabbb7b1b88890995515cb9f65bf
onlymachiavelli <tmakaveli643@gmail.com>
2022-01-15 20:41:03 +0100

    ex

--- ALGO/Classe/rec/ex.py
+++ ALGO/Classe/rec/ex.py
@@ -1,21 +1,57 @@
-m = [
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-]
+import numpy as np
+import random as r
+
+# Partie Size
 
 
-for i in range(len(m)):
-    for j in range(i+1):
-        if j == 0 or i == j:
-            m[i][j] = 1
-        else:
-            m[i][j] = m[i-1][j] + m[i-1][j-1]
+def size():
+    quit = False
+    while not quit:
+        n = int(input("Give N "))
+        quit = 20 < n < 120
+    return n
 
 
-for i in range(len(m)):
-    print(m[i])
+def Range(msg: str):
+    quit = False
+    while not quit:
+        num = int(input("Enter Num  " + msg))
+        quit = -100 <= num <= 100
+    return num
 
 
-print(2**5)
+def trouve(t, n: int, num: int):
+    test = False
+    i = 0
+    while not test and i < n:
+        test = t[i] == num
+        i += 1
+    return test
+
+
+def remplir(n: int, minn: int, maxx: int, t):
+    for i in range(n):
+        quit = True
+        while quit:
+            num = r.randint(minn, maxx)
+            quit = trouve(t, i, num)
+        t[i] = num
+
+
+n = size()
+arr = np.zeros(n, dtype=int)
+
+
+print(arr)
+quit = False
+
+while not quit:
+    minn = Range("min")
+    maxx = Range("max")
+    print(maxx > minn)
+    print(maxx - minn)
+    quit = maxx > minn and maxx-minn >= n
+
+remplir(n, minn, maxx, arr)
+
+print(arr)
