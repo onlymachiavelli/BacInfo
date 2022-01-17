bce06ff4ac1f4d9fe82295dcc4192b9aca985bb7
onlymachiavelli <tmakaveli643@gmail.com>
2022-01-17 17:10:46 +0100

    ex

--- ALGO/javfeb/problem1.py
+++ ALGO/javfeb/problem1.py
@@ -26,18 +26,34 @@ def fillFile(src: str, n: int, i: int = 0):
             fillFile(src, n, i+1)
 
 
-def Fact(n: int, i: int = 2):
-    if n == 1:
-        return ""
-    if n % i != 0:
-        return Fact(n, i+1)
+def Fact(n: int, arr: list, i: int = 2):
+    if n > 1:
+        print(n)
+        print(arr)
+        if n % i != 0:
+            Fact(n, arr, i+1)
+        else:
+            arr.append(i)
+            Fact(n/i, i)
+
+
+def Frequency(ch: list, c: int = 1, n: int = 0):
+    if len(ch) > 0:
+        if ch[n] == ch[n-1]:
+            return Frequency(ch, c+1, n-1)
+        else:
+            return str(c)+ch[n-1] + Frequency(ch, 1, n-1)
     else:
-        return str(i) + Fact(n/i, i)
-
-
-print(Fact(1912))
+        return ""
 
 
 def fillRes(src: str, n: int, i: int = 0):
+
     if i < n:
         myFile = open(src, "w")
+
+
+fa = []
+print(Fact(1912, fa))
+
+print(fa)
