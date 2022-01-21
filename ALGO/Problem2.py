881d958360e83b38ee8077518f0e1eb8b6401378
onlymachiavelli <tmakaveli643@gmail.com>
2022-01-21 23:14:13 +0100

    ex

---
+++ ALGO/Problem2.py
@@ -0,0 +1,97 @@
+import random as r
+import numpy as np
+
+
+def size():
+    print("Enter N")
+    n = int(input())
+    if 4 < n < 20:
+        return n
+    return size()
+
+
+def isPrime(n: int, i: int = 2):
+    if n > 1:
+        if i <= n//2:
+            if n % i != 0:
+                return isPrime(n, i+1)
+            else:
+                return False
+        return True
+    else:
+        return False
+
+
+def fillMat(n: int, mat, i: int = 0, j: int = 0):
+    if i < n and j < n:
+        num = r.randint(2, 99)
+        columnIndex = j
+        LineIndex = i
+        if j == n-1:
+            columnIndex = 0
+            LineIndex = i+1
+        else:
+            columnIndex = j+1
+        if isPrime(num):
+            mat[i][j] = num
+            fillMat(n, mat, LineIndex, columnIndex)
+        else:
+            fillMat(n, mat, i, j)
+
+
+def isSorted(m, n: int, l: int, isLine: bool):
+    j = 0
+    checkIncr = True
+    checkDecr = True
+    theString = ""
+    while j <= n-2:
+        if isLine:
+            First = m[l][j]
+            Then = m[l][j+1]
+        else:
+            First = m[j][l]
+            Then = m[j+1][l]
+        if First > Then:
+            checkDecr = False
+        else:
+            checkIncr = False
+        if checkIncr or checkDecr:
+            theString += str(First) + "-"
+        else:
+            theString = ""
+        j += 1
+    return theString
+
+
+def genResult(m, n: int, src: str, i: int = 0):
+    if i < n:
+        myFile = open(src, "a")
+        if len(isSorted(m, n, i, False)) > 0:
+            myFile.write("C" + str(i) + "*" + isSorted(m,
+                         n, i, False) + str(m[i][n-1])+"\n")
+        if len(isSorted(m, n, i, True)) > 0:
+            myFile.write("L" + str(i) + "*" + isSorted(m,
+                         n, i, True) + str(m[i][n-1])+"\n")
+        myFile.close()
+        genResult(m, n, src, i+1)
+
+
+n = size()
+mat = np.zeros([n, n], dtype=int)
+fillMat(n, mat)
+print(mat)
+genResult(mat, n, "result.txt")
+
+"""
+si tu veux teste le donnee de l'excercice : 
+m = [
+  [2,73,79,5,23],
+  [5,29,37,61,67],
+  [23,17,43,53,59],
+  [29,23,47,7,89],
+  [31,29,23,19,17]
+]
+n = len(m)
+
+    
+"""
--- TIC/toExecute,.sql
+++ TIC/toExecute,.sql
@@ -1,1 +1,9 @@
---to create 
+create table livre (
+    codelivre primary key int(4) ,
+    titre varchar (100), 
+    auteur enum ('claude duigo', 'eric sarrion','christine berhardt' ), 
+    annee varchar(4)  ,   
+    nbrpage int(3)
+
+) engine = innodb;
+
