061eae08d1c5d6324ab3020d380bf0716fb316fb
onlymachiavelli <tmakaveli643@gmail.com>
2022-01-26 22:40:07 +0100

    ex

--- ALGO/dv.c
+++ ALGO/dv.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int main()
+int size()
 {
+    int n;
+    printf("Enter Number! \n ");
+    scanf("%d", &n);
+    if ((40 < n) && (n < 100))
+    {
+        return n;
+    }
+    return size();
+}
 
-    return 0;
+main()
+{
 }
