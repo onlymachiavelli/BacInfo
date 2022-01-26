7dc74726573a1b9108bf12830ac531c9cd8a62c1
onlymachiavelli <tmakaveli643@gmail.com>
2022-01-26 22:57:56 +0100

    ex

--- ALGO/dv.c
+++ ALGO/dv.c
@@ -12,7 +12,29 @@ int size()
     }
     return size();
 }
+char *boolFuck(bool func)
+{
+    return (func ? "true" : "false");
+}
 
+bool isPrime(int n, int i)
+{
+    if (n > 1)
+    {
+        if (i <= n / 2)
+        {
+            if (n % i == 0)
+                return false;
+            return isPrime(n, i + 1);
+        }
+        return true;
+    }
+    return false;
+}
+void superPrime()
+{
+}
 main()
 {
+    printf("%s ", boolFuck(isPrime(4, 2)));
 }
