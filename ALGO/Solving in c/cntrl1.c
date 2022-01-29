26df266fed4929a5438bb284114e2dad7e833fe0
onlymachiavelli <tmakaveli643@gmail.com>
2022-01-29 22:30:44 +0100

    renaming some files

---
+++ ALGO/Solving in c/cntrl1.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+
+int main (){
+
+    FILE *test;
+    test = fopen("test.dat", "wb");
+    int a = 5;
+    int b = 7;
+    fwrite(a, b , a+b , test);
+
+    fclose(test);
+
+
+    test=  fopen("test.dat", "rb");
+    char aa[70];
+    while (fscanf(test, "%d %d %d", aa) != EOF){
+        printf(a);
+    }
+    fclose(a);
+    return 0 ;
+}
