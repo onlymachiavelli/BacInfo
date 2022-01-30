99e3cc868026a3514761915f69ec0d0d42eb90d3
onlymachiavelli <tmakaveli643@gmail.com>
2022-01-30 14:59:42 +0100

    ex in c

--- ALGO/Solving in c/cntrl2.c
+++ ALGO/Solving in c/cntrl2.c
@@ -1,22 +1,27 @@
 #include <stdio.h>;
 #include <stdbool.h>;
 
+char *boolString(bool var) return var ? "true" : "false";
 
 int defN() {
     int n ;
     printf("Enter N ! \n");
     scanf("%d", &n) ;
-    if (400 < n <  1000) return n;
-    else return defN();
+    if ((400 < n) && ( n<  1000))return n;
+    return defN();
 }
 int defM(int n ) {
     int m ;
-    return m ;
+    printf("Enter M \n");
+    scanf("%d", &m);
+    if ((n>m) && (m>400)) return m ;
+    return defM(n);
 }
+
 
 int main () {
 
-    int n = defN();
+    int n = defN(), m = defM(n);
     printf("hello world");
     return 0;
 }
