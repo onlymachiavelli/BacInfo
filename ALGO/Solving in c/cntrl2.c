da2deda81d8323854568d4fc42bce6e4ebc18107
onlymachiavelli <tmakaveli643@gmail.com>
2022-02-01 01:08:08 +0100

    ex

--- ALGO/Solving in c/cntrl2.c
+++ ALGO/Solving in c/cntrl2.c
@@ -1,68 +1,79 @@
-#include <stdio.h>;
-#include <stdbool.h>;
+#include <stdio.h>
+#include <stdbool.h>
 
-char *boolString(bool var){ return (var ? "true" : "false");}
+char *boolString(bool var) { return (var ? "true" : "false"); }
 
-int defN() {
-    int n ;
+int defN()
+{
+    int n;
     printf("Enter N ! \n");
-    scanf("%d", &n) ;
-    if ((400 < n) && ( n<  1000))return n;
+    scanf("%d", &n);
+    if ((400 < n) && (n < 1000))
+        return n;
     return defN();
 }
-int defM(int n ) {
-    int m ;
+int defM(int n)
+{
+    int m;
     printf("Enter M \n");
     scanf("%d", &m);
-    if ((n>m) && (m>400)) return m ;
+    if ((n > m) && (m > 400))
+        return m;
     return defM(n);
 }
 
-int isPrime(int n , int i ){
-    if (n > 1 ) {
-        if (i >= n/2 ) return true;
-        if (n%i == 0) return false;
-        return isPrime(n, i+1);
+int isPrime(int n, int i)
+{
+    if (n > 1)
+    {
+        if (i >= n / 2)
+            return true;
+        if (n % i == 0)
+            return false;
+        return isPrime(n, i + 1);
     }
-    return false ;
+    return false;
 }
 
-char *primeStuffs(int n){
-    int res = 1, i=2 ;
-    char*result , fuck;
-    char op[1]= "";
-    while (res!= n){
-        if (isPrime(i, 2)){
+char *primeStuffs(int n)
+{
+    int res = 1, i = 2;
+    char *result, *fuck, *op = "";
+    while (res != n)
+    {
+        printf("%s \n", result);
+        if (isPrime(i, 2))
+        {
             res *= i;
             sprintf(fuck, "%d *", i);
-            result += fuck ;
+            // result += fuck;
+            sprintf("%s %s ", result, fuck);
         }
 
-        if (res+1 == n ){
-            op[0] = "+";
+        if (res + 1 == n)
+        {
+            op = "+";
             res++;
         }
-        else {
-            op[0]= "-";
-            res -- ;
+        else
+        {
+            op = "-";
+            res--;
         }
-
-
 
         i++;
     }
-    result = "#" + fuck + op + result ;
+    // result = "#" + fuck + op + result ;
+    sprintf(result, "# %s %s %s", fuck, op, result);
     return result;
 }
-int main () {
+int main()
+{
+    printf("fuck you");
+    int q = 3;
+    q += 4;
+    printf("%d", q);
+    int i;
 
-    printf("%s /n", boolString(isPrime(9, 2)));
-    int n = defN(), m = defM(n);
-
-
-    int i ;
-    for (i = m; i<=n;i++){
-        printf("%s \n" ,  primeStuffs(i));
-    }
     return 0;
 }
