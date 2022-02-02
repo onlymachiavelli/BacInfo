726a5e5e523f6ace7b8d2b090eebd6e8c26db0b2
onlymachiavelli <tmakaveli643@gmail.com>
2022-02-02 21:56:19 +0100

    ex

---
+++ ALGO/Classe/inFeb/Suite&MathStuffs/binarySuite.py
@@ -0,0 +1,17 @@
+from cgitb import reset
+from numpy import binary_repr
+
+
+def binary(n: int, i: int = 0, suite: str = "0"):
+    if n > 0:
+        if i < n:
+            if suite[i] == "0":
+                toReturn = suite[0:len(suite)] + "01"
+            else:
+                toReturn = suite[0:len(suite)] + "10"
+            return binary(n, i+1, toReturn)
+        return suite
+    return suite
+
+
+print(binary(3))
