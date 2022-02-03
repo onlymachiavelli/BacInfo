90f43de629b5909b7fc59c0b268cdb748a209506
onlymachiavelli <tmakaveli643@gmail.com>
2022-02-03 22:06:34 +0100

    ex c

--- ALGO/Classe/inFeb/SuiteMathStuffs/binarySuite.py
+++ ALGO/Classe/inFeb/SuiteMathStuffs/binarySuite.py
@@ -2,13 +2,15 @@
 
 def binary(n: int, i: int = 0, suite: str = "0"):
     if n > 0:
-        if i < n:
+        if i < len(suite):
+            toReturn = ""
             if suite[i] == "0":
-                toReturn = suite[0:len(suite)] + "01"
+                toReturn = suite[0:i-1] + "01" + suite[i: len(suite)]
             else:
-                toReturn = suite[0:len(suite)] + "10"
+                toReturn = suite[0:i-1] + "10" + suite[i: len(suite)]
             return binary(n, i+1, toReturn)
-        return suite
+        else:
+            return binary(n-1, 0, suite)
     return suite
 
 
