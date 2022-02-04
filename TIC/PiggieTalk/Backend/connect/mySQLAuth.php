20077824b988f54ee390d5940878378a21f3e7d5
onlymachiavelli <tmakaveli643@gmail.com>
2022-02-04 02:15:21 +0100

    piggietalk connect server auth

--- TIC/PiggieTalk/Backend/connect/mySQLAuth.php
+++ TIC/PiggieTalk/Backend/connect/mySQLAuth.php
@@ -3,6 +3,7 @@
 function makeAuth($hostname, $username, $password, $dbName) {
     try{
         $authSql = new mysqli($hostname, $username, $password);
+        return array([TRUE , $authSql]);
     }
     catch(e){
         return array([FALSE, e]);
