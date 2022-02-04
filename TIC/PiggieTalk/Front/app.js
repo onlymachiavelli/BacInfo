ee736a91c39853a2a329ec4376f1ca0c0a665a7e
onlymachiavelli <tmakaveli643@gmail.com>
2022-02-04 02:08:11 +0100

    piggietalk UI login

---
+++ TIC/PiggieTalk/Front/app.js
@@ -0,0 +1,7 @@
+const Id = (id) => document.getElementById(id)
+const Class = (cl, ind) => document.getElementsByClassName(cl)[ind]
+const Name = (nam, id) => document.getElementsByName(nam)[id]
+
+const app = () => {
+  return 0
+}
