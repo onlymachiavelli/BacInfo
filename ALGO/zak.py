2f71f45d71dfc84f1c302b7fba1a7e9193a543ed
onlymachiavelli <tmakaveli643@gmail.com>
2022-02-05 23:24:42 +0100

    ex

--- ALGO/zak.py
+++ ALGO/zak.py
@@ -1,8 +1,14 @@
 import random as r
+from tty import setcbreak
 
 
 def check(myString: str, n: int, ele: str):
-    return True
+    check = True
+    i = 0
+    while check and i < n:
+        check = not myString == ele
+    i += 1
+    return check
 
 
 def word():
@@ -15,3 +21,22 @@ def word():
 
 def genAlpha():
     alpha = "abcdefghijjklmnopqrstuvwxyz"
+    newAlpha = ""
+    for i in range(26):
+        quit = False
+        while not quit:
+            st = alpha[r.randint(0, 25)]
+            quit = check(newAlpha, i, st)
+        newAlpha += st
+    return newAlpha
+
+
+def secret():
+    alpha = "abcdefghijklmnopqrstuvwxyz"
+    quit = False
+    while not quit:
+        secret = input("Enter Secret ")
+        i = 0
+        while i < len(secret) and not quit:
+            check = check(secret, len(secret), secret[i])
+            i += 1
