b41e7cb3df338d23ab55fc2e9d2a2e585478236f
onlymachiavelli <tmakaveli643@gmail.com>
2022-02-05 22:17:37 +0100

    ex

--- ALGO/Solving in c/try.c
+++ ALGO/Solving in c/try.c
@@ -1,35 +1,13 @@
 #include <stdio.h>
 #include <string.h>
-char *sub(char *myString, int from, int too)
-{
-    char *newString = "";
-    for (int i = from; i <= too; i++)
-    {
-        newString += myString[i];
-    }
 
-    return newString;
-}
-
-char *shit(int n, char *suite, int i)
+char *suite(int n, char *myString, int i)
 {
-    char *toReturn;
-    if (n > 0)
-    {
-        if (i < strlen(suite))
-        {
-            return 0;
-        }
-        return shit(n - 1, suite, 0);
-    }
-    return suite;
+    return "";
 }
 
 int main()
-
 {
-    char *name = "alaa barka";
-    printf("Hello world \n");
-    printf("%s", sub(name, 1, 5));
+
     return 0;
 }
--- ALGO/zak.py
+++ ALGO/zak.py
@@ -1,3 +1,17 @@
-nom = "Alaa"
-nom = "g" + nom[1:len(nom)-1]
+import random as r
 
+
+def check(myString: str, n: int, ele: str):
+    return True
+
+
+def word():
+    quit = False
+    while not quit:
+        word = input("Enter the word ")
+        quit = word.isupper()
+    return word
+
+
+def genAlpha():
+    alpha = "abcdefghijjklmnopqrstuvwxyz"
