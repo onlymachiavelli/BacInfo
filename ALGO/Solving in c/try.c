0e7dec77f1f46425b92d9fdd733a0c9ab93ffc89
onlymachiavelli <tmakaveli643@gmail.com>
2022-02-05 10:28:29 +0100

    ex

--- ALGO/Solving in c/try.c
+++ ALGO/Solving in c/try.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
+#include <string.h>
+char *sub(char *myString, int from, int too)
+{
+    char *newString = "";
+    for (int i = from; i <= too; i++)
+    {
+        newString += myString[i];
+    }
+
+    return newString;
+}
+
+char *shit(int n, char *suite, int i)
+{
+    char *toReturn;
+    if (n > 0)
+    {
+        if (i < strlen(suite))
+        {
+            return 0;
+        }
+        return shit(n - 1, suite, 0);
+    }
+    return suite;
+}
 
 int main()
+
 {
-    char *shit = "hello world";
-    printf("%s", shit[0]);
+    char *name = "alaa barka";
+    printf("Hello world \n");
+    printf("%s", sub(name, 1, 5));
+    return 0;
 }
