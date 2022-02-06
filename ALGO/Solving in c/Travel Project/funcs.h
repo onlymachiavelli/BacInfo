4c4cadc2df77b3cdf871cc6e919b38c317e9aaac
onlymachiavelli <tmakaveli643@gmail.com>
2022-02-06 20:32:26 +0100

    ex

---
+++ ALGO/Solving in c/Travel Project/funcs.h
@@ -0,0 +1,5 @@
+
+int age()
+{
+    return 23;
+}
---
+++ ALGO/Solving in c/Travel Project/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int travelers(char *source)
+{
+
+    FILE *shit;
+    int n = 0;
+    shit = fopen("data.dat", "rb");
+    if (shit != NULL)
+    {
+    }
+
+    return n;
+}
+
+int main()
+{
+    return 0;
+}
