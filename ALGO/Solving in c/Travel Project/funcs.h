d4790e406e6c491e837b994117c53c15617c45e0
onlymachiavelli <tmakaveli643@gmail.com>
2022-02-09 16:07:26 +0100

    tr

---
+++ ALGO/Solving in c/Travel Project/funcs.h
@@ -0,0 +1,7 @@
+typedef struct  Travelers {
+    char* ID;
+    char* name;
+    char* adress;
+    int total;
+
+}Travelers;
---
+++ ALGO/Solving in c/Travel Project/main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include "funcs.h"
+
+
+
+int main(){
+    char* fileSoource = "shit.dat";
+    int dataSize = 0;
+    int answer;
+    bool check;
+    Travelers array[100];
+
+    char* Menu = "Menu Control\n1:Total Traverlers\n2:Fill Traveler\n3:Charge Data\n4:Find Traveler\n5:Add Traveler\n6:Delete Traveler\n7:Libr\n";
+
+    //show the menu
+    printf(Menu);
+    while (true){
+        check = false;
+        //verify the answer
+        while (!check) {
+            printf("Enter the option");
+            scanf("%d", answer);
+            check = answer >= 1 && answer <= 7;
+        }
+        if (answer < 9) {
+
+            switch (answer) {
+            case 1: break;
+
+            case 2: break;
+
+            case 3: break;
+
+            case 4: break;
+
+            case 5: break;
+
+            case 6:
+                break;
+
+            case 7:
+                break;
+
+            }
+        }
+        else printf("Clossing the program\n"); break;
+    }
+    return 0;
+
+
+
+}
