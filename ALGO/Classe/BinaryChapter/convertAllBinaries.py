920d22d22912e790333a71a4ae166f74dafd4a15
onlymachiavelli <tmakaveli643@gmail.com>
2022-02-18 21:30:34 +0100

    Exercice 2 bin wedhni

--- ALGO/Classe/BinaryChapter/convertAllBinaries.py
+++ ALGO/Classe/BinaryChapter/convertAllBinaries.py
@@ -9,15 +9,15 @@ def getS(num: int):
 
 
 def fromDec(code: int, base: int):
-    if code > 0:
+    if code >= 0:
         return fromDec(code // base, base) + getS(code % base)
     return ""
-
-
-print(fromDec(740, 2))
 
 
 def toDec(code: str, base: int, i: int, j: int = 0):
     if i >= 0:
         return (getN(code[j])*(base**i)) + toDec(code, base, i-1, j+1)
     return 0
+
+
+print(fromDec("1011100100", 16))
---
+++ ALGO/Classe/BinaryChapter/serie1Ex1.py
@@ -0,0 +1,40 @@
+def check(myString: str):
+    fuck = True
+    for i in range(len(ch)):
+        if myString[i] != "0" and myString[i] != "1":
+            fuck = False
+    return fuck
+
+
+def fillBin():
+    myString = str(input("Enter your binary Code ! : "))
+    if check(myString):
+        return myString
+    return fillBin()
+
+
+def toDec(code: str, base: int, i: int, j: int = 0):
+    if i >= 0:
+        return (int(code[j]))*(base**i) + toDec(code, base, i-1, j+1)
+    return 0
+
+
+def splitShit(myString: str):
+    loc = myString
+    h = ""
+    while len(loc) % 4 != 0:
+        loc = "0" + loc
+    print(loc)
+    while len(loc) > 0:
+        x = toDec(loc[0:4], 2, 3)
+        if x <= 9:
+            c = str(x)
+        else:
+            c = chr(x + 55)
+        h += c
+        loc = loc[4:len(loc)]
+    print(h)
+
+
+myString = fillBin()
+splitShit(myString)
