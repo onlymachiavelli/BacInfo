34a63d249ba89316999cdd98b6ecb519d05d13ec
onlymachiavelli <tmakaveli643@gmail.com>
2022-02-18 18:42:14 +0100

    random func

--- ALGO/Solving in c/encodingChapter/ex1.c
+++ ALGO/Solving in c/encodingChapter/ex1.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <string.h>
-
+#include <stdlib.h>
+#include <time.h>
+int random(int lower , int higher){
+    int toReturn = 0;
+    if (higher > lower ) toReturn = rand() % (higher - lower +1 ) + 1 ;
+    return toReturn; 
+}
+char *getString(int code , int base)  {
+    
+     
+}
+int getNumber(char *code , int base){}
 int main () {
-    printf("Hello woorld") ;
-    return 0 ;
+    srand(time(0));
+    for(int i=0;i<6;i++) printf("%d \n" ,  random(20, 99) );
 }
