2585610cfabd1397cdc3a199cf352653006a2e0c
onlymachiavelli <tmakaveli643@gmail.com>
2022-02-25 21:47:54 +0100

    ex

--- ALGO/Classe/BinaryChapter/shit.py
+++ ALGO/Classe/BinaryChapter/shit.py
@@ -3,3 +3,15 @@ def size(minimum: int, maximum: int):
     if minimum < n < maximum:
         return n
     return size(minimum, maximum)
+
+
+def isPrime(n : int , i:int = 2):
+    if n > 1 :
+        if i <= n//2 :
+            return n % i != 0 and isPrime(n , i+1) 
+        return True  
+    return False
+
+
+def result () : 
+    return 0 
