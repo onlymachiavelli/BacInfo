348839d145c689d7ad14c4d8c0c6cee51cb5961c
onlymachiavelli <tmakaveli643@gmail.com>
2022-02-25 21:50:37 +0100

    ex

--- ALGO/Classe/BinaryChapter/shit.py
+++ ALGO/Classe/BinaryChapter/shit.py
@@ -5,13 +5,27 @@ def size(minimum: int, maximum: int):
     return size(minimum, maximum)
 
 
-def isPrime(n : int , i:int = 2):
-    if n > 1 :
-        if i <= n//2 :
-            return n % i != 0 and isPrime(n , i+1) 
-        return True  
+def isPrime(n: int, i: int = 2):
+    if n > 1:
+        if i <= n//2:
+            return n % i != 0 and isPrime(n, i+1)
+        return True
     return False
 
 
-def result () : 
-    return 0 
+def result(a: int, b: int):
+    myFile = open("result.txt", "w")
+    for i in range(a, b+1):
+        pre = 2
+        res = 0
+        check = False
+        while res < i:
+            if not isPrime(pre):
+                pre += 1
+            else:
+                res = (2 ** pre) - 1
+        if check:
+            myFile.write(str(i) + " Est mersenne ")
+        else:
+            myFile.write(str(i) + " Est non  mersenne ")
+    myFile.close()
