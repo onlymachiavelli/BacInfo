359c942243da466dc584abbe954bd9b47e6236f6
onlymachiavelli <tmakaveli643@gmail.com>
2022-02-26 19:58:32 +0100

    exam file

--- ALGO/Exams/tr2/dvSynth2.py
+++ ALGO/Exams/tr2/dvSynth2.py
@@ -1,1 +1,15 @@
-print("Hello world")
+from unicodedata import decimal
+import numpy as np
+
+
+def size(minimum: int, maximum: int):
+    n = int(input("Enter N "))
+    if minimum < n < maximum:
+        return n
+    return size(minimum, maximum)
+
+
+def fromDecimal(code: int, base: int):
+    if code >= 0:
+        return str(code % base) + fromDecimal(code // base, base)
+    return ""
