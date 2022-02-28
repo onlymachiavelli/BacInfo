7f5c47c0f4486ca1729f92b81a95b7932c99f91f
onlymachiavelli <tmakaveli643@gmail.com>
2022-02-28 14:14:21 +0100

    dv ex

--- ALGO/Exams/tr2/dvSynth2.py
+++ ALGO/Exams/tr2/dvSynth2.py
@@ -6,3 +6,22 @@ def size(minimum: int, maximum: int):
     if minimum < n < maximum:
         return n
     return size(minimum, maximum)
+
+
+def fillMatrix(matrix: np.ndarray, l: int, c: int):
+    myFile = open("toCrypt.txt", "r")
+    i = 0
+    j = 0
+    check = False
+    while i < l and j < c:
+        line = myFile.readline()
+        if line != None:
+            check = False
+        for k in range(len(line)):
+            if line[k] == "\n":
+                matrix[i, j] = "#"
+            if j == c - 1:
+                j = 0
+                i += 1
+
+    myFile.close()
