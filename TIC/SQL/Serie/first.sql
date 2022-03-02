69763c4c607e43c9ea0536a2842a2d0ed1ecb951
onlymachiavelli <tmakaveli643@gmail.com>
2022-03-02 23:12:01 +0100

    SME SQL CODES

---
+++ TIC/SQL/Serie/first.sql
@@ -0,0 +1,9 @@
+CREATE TABLE CITOYEN (
+    Cin VARCHAR (20) PRIMARY KEY ,
+    Nom VARCHAR (20) NOT NULL , 
+    Prenom VARCHAR(20) NOT NULL , 
+    theDate DATE NULL, 
+    genre VARCHAR(30) , 
+    ville VARCHAR(20 ) ,
+    Tel VARCHAR(81)  
+); 
