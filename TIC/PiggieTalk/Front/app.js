19dd2aae09b0c9b6bfa10d6f7bfae722cf3e498a
onlymachiavelli <tmakaveli643@gmail.com>
2022-03-03 19:38:59 +0100

    preparing input check func

--- TIC/PiggieTalk/Front/app.js
+++ TIC/PiggieTalk/Front/app.js
@@ -1,14 +1,14 @@
 const Id = (id) => document.getElementById(id)
 const Class = (cl, ind) => document.getElementsByClassName(cl)[ind]
 const Name = (nam, id) => {
-  return id
+  return id >= 0
     ? document.getElementsByName(nam)[id]
     : document.getElementsByName(nam)
 }
 
 const checkSignUp = () => {
-  const Arr = Name("sex").value
-  console.log(Arr)
+  const Arr = Name("sex", -1).value
+  console.log(Arr[0].checked)
 }
 
 const checkLogin = () => {}
