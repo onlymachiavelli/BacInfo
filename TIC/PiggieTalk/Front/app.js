336ca56255d038237ddf55c8f0d488286f83c4ab
onlymachiavelli <tmakaveli643@gmail.com>
2022-03-04 02:07:23 +0100

    pb

--- TIC/PiggieTalk/Front/app.js
+++ TIC/PiggieTalk/Front/app.js
@@ -1,16 +1,9 @@
 const Id = (id) => document.getElementById(id)
 const Class = (cl, ind) => document.getElementsByClassName(cl)[ind]
-const Name = (nam, id) => {
-  return id >= 0
-    ? document.getElementsByName(nam)[id]
-    : document.getElementsByName(nam)
-}
-
+const Name = (nam) => document.getElementsByName(nam)
 const checkSignUp = () => {
-  const Arr = Name("sex", -1).value
-  console.log(Arr[0].checked)
+  const arr = Name("sex").value
+  console.log(arr)
 }
 
 const checkLogin = () => {}
-
-checkSignUp()
