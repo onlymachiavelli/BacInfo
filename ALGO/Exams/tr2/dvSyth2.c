5399471f3ba4194ed63051618bac1acb80a2e92a
onlymachiavelli <tmakaveli643@gmail.com>
2022-03-06 20:25:32 +0100

    took the fckign lines

--- ALGO/Exams/tr2/dvSyth2.c
+++ ALGO/Exams/tr2/dvSyth2.c
@@ -2,12 +2,27 @@
 #include <stdbool.h>
 #include <string.h>
 
-int main() {
-    FILE* myFile = fopen("toCrypt.txt", "r");
+void fillMatrix(char* source, int l, int c, char matrix[l][c]) {
+    FILE* myFile = fopen(source, "r");
     char* line;
+    int i = 0, j = 0;
     while (fgets(line, sizeof(line), myFile)){
-        printf(line);
+        for (int k = 0;k < strlen(line);k++) {
+            matrix[i][j] = line[k];
+            j++;
+            if (j == c) {
+                j = 0;
+                i++;
+            }
+        }
     }
+
     fclose(myFile);
+
+}
+int main() {
+
+    int l, c;
+    char matrix[l][c];
     return 0;
 }
