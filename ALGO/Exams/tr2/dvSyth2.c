65e4dddd131b1e4fd0d902e7a6c7b78ca5efa77f
onlymachiavelli <tmakaveli643@gmail.com>
2022-03-07 17:05:26 +0100

    st

--- ALGO/Exams/tr2/dvSyth2.c
+++ ALGO/Exams/tr2/dvSyth2.c
@@ -1,35 +1,49 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
 
+int size(int min, int max) {
+    printf("Entert N \n");
+    int n;
+    scanf("%d", &n);
+    if (n > min && n < max) return n;
+    return size(min, max);
+
+}
 void fillMatrix(char* source, int l, int c, char matrix[l][c]) {
-    FILE* myFile = fopen(source, "r");
+    FILE* myFile = open(source, "r");
     char* line;
-    bool backLine = false;
     int i = 0, j = 0;
     while (fgets(line, sizeof(line), myFile)){
-
-        for (int k = 0;k < strlen(line);k++) {
-            matrix[i][j] = line[k];
-            j++;
-            if (j == c) {
-                j = 0;
-                i++;
-            }
+        for (int k = 0;k < strlen(line);k++){
+            i += 0;
         }
-        matrix[i][j] = "#";
-
-
-    }
-    while (i < l && j < c) {
-        matrix[i][j] = "*";
     }
     fclose(myFile);
-
 }
 int main() {
 
+    FILE* file = open("toCrypt.txt", "r");
+    char* line;
+    bool check;
+    while (fgets(line, sizeof(line), file)){
+        if (line[strlen(line)] == "\n"){
+            printf("Yes @ \n");
+        }
+        else {
+            printf("nope");
+        }
+    }
+    fclose(file);
+    //shits
     int l, c;
+    while (true) {
+        l = size(7, 20);
+        c = size(l, 20);
+        if (l * c >= 60)  break;
+    }
+
     char matrix[l][c];
-    return 0;
+
+
 }
