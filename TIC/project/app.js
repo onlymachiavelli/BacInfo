94edcdd89e06473246b1bde2a20be8c27e38df61
onlymachiavelli <tmakaveli643@gmail.com>
2022-04-01 23:46:34 +0100

    algo + tics

--- TIC/project/app.js
+++ TIC/project/app.js
@@ -6,7 +6,7 @@ const check = () => {
     nom: Name("name")[0].value,
     gen: Name("sex"),
     email: Name("email")[0].value,
-    clas: Name("nc"),
+    clas: Name("nc")[0].value,
   }
   let error = ""
   if (myDatas.nom.length <= 4 || myDatas.nom.indexOf(" ") === -1) {
@@ -16,16 +16,26 @@ const check = () => {
     error += "you have to choose your sex ! \n"
   }
 
-  const [dom, com] = myDatas.email.split(".")
-  const [user, platform] = myDatas.email.split("@")
-  if (
-    !(com.toLowerCase() in ["fr", "com"]) ||
-    user.length < 4 ||
-    !(platform.toLowerCase() in ["gmail", "outlook", "yahoo", "live", "proton"])
-  ) {
+  if (myDatas.email.indexOf("@") !== -1 && myDatas.email.indexOf(".") !== -1) {
+    let [dom, com] = myDatas.email.split(".")
+    let [user, platform] = myDatas.email.split("@")
+    platform = platform.split(".")[0]
+    console.log(dom, com, user, platform)
+    if (
+      (!(com.toLowerCase() in ["fr", "com"]) && user.length < 4) ||
+      !(
+        platform.toLowerCase() in
+        ["gmail", "outlook", "yahoo", "live", "proton"]
+      )
+    ) {
+      error += "email is not valid \n"
+    }
+  } else {
     error += "email is not valid \n"
   }
-
+  if (myDatas.clas === null) {
+    error += "select your class !  \n"
+  }
   if (error.length > 0) {
     alert(error)
   }
@@ -33,10 +43,8 @@ const check = () => {
 }
 
 const Click = () => {
-  if (!check()) {
-    alert("Form is not valid")
-  } else {
-    alert("good")
+  if (check()) {
+    alert("Good")
   }
 }
 
