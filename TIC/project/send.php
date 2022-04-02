18fc2f7dc69d802aea091b25e8423b6a6d5ae6e7
onlymachiavelli <tmakaveli643@gmail.com>
2022-04-02 01:08:15 +0100

    requests stuffs

--- TIC/project/send.php
+++ TIC/project/send.php
@@ -1,7 +1,36 @@
 <?php
 //connect 
+$localhost = "localhost";
+$username = "root";
+$password = "";
+mysql_connect($localhost,$username,$password);
 
-echo "hello world";
+$fullname = $_POST['name'];
+$name = "";
+$lname = "" ;
+for ($i=0;$i<strlen($fullname);$i++){
+    if ($fullname[$i] == " "){
+        $name = substr($fullname,0,$i);
+        $lname = substr($fullname,$i+1,strlen($fullname));
+        break;
+    }
+}
 
 
+$reqDatas = array(
+    "name" =>$name,
+    "lname" =>$lname,
+    "sex" =>$_POST['sex'],
+    "email" =>$_POST['email'],
+    "bac" =>$_POST['nc'],
+);
+
+foreach($reqDatas as $key => $value) {
+    echo "$value <br/>" ;
+}
+//select database
+mysql_select_db("formation");
+
+$req = "INSERT INTO CONDIDAT (nom,prenom,genre,email,bac) VALUES ('".$reqDatas['name']."','".$reqDatas['lname']."','".$reqDatas['sex']."','".$reqDatas['email']."','".$reqDatas['bac']."')";
+mysql_query($req);
 ?>
