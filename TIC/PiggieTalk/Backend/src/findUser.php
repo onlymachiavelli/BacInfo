d943238f493ee6e2705224dd730b0595b5fca3e0
onlymachiavelli <tmakaveli643@gmail.com>
2022-04-03 03:55:06 +0100

    stffs

--- TIC/PiggieTalk/Backend/src/findUser.php
+++ TIC/PiggieTalk/Backend/src/findUser.php
@@ -4,7 +4,6 @@ require ('connect.php');
 
 
 function findUser($data) {
-    connectDB();
     $req = "SELECT username FROM users WHERE username = '$data'";
     $res = query($req) ;
     return $res -> num_rows > 0 ? FALSE : TRUE ;
--- TIC/PiggieTalk/Backend/src/signUp.php
+++ TIC/PiggieTalk/Backend/src/signUp.php
@@ -1,6 +1,6 @@
 <?php
 
-require ('connect.php');
+require ('./connect.php');
 require ('caesar.php');
 require ('findUser.php');
 connectDB();
@@ -15,9 +15,12 @@ $schema = array (
     "name" => $_POST['fname'] . " " . $_POST['lname'],
     "birthday" => $_POST['date'],
     "password" => $passsword,
+    "created_at" => date("Y-m-d H:i:s"),
     "sex" => $_POST['sex'] 
 );
 
+echo $schema['created_at'];
+$req = "INSERT INTO users (username, fullname,email , password , created_at,)";
 
 
 ?>
