8f0f2a7f6c73baa9360663a22abb6892fd7d7492
onlymachiavelli <tmakaveli643@gmail.com>
2022-04-12 21:33:33 +0100

    table done

---
+++ TIC/project/delete.php
@@ -0,0 +1,10 @@
+<?php
+if (isset($_POST['id']))
+{
+
+}
+else{
+    echo "Error getting datas ! ";
+}
+
+?>
--- TIC/project/fetchDatas.php
+++ TIC/project/fetchDatas.php
@@ -1,11 +1,10 @@
 <?php
-//connecting
 
-    mysql_connect($localhost,$username,$password) or die("Error connecting to phpMyadmin");
+    mysql_connect("localhost","root","") or die("Error connecting to phpMyadmin");
     mysql_select_db("formation") or die("Error selecting the database") ;
 
-
-
+    $req = "SELECT * FROM condidat";
+    $res = mysql_query($req) ;
 
 ?>
 
@@ -14,23 +13,49 @@
     <head>
         <meta charset="UTF-8" />
         <title>Getting all the datas !</title>
-        
+        <style>
+            table{width:100%;}
+            table, th, tr, td {
+                border:1px solid black;
+                border-collapse: collapse;
+            }
+            td, th{
+                padding:10px;
+            }
+        </style>
     </head>
     <body>
-        <h1>All the dayas ! </h1>
+        <h1>All the datas ! </h1>
 
         <br/>
 
         <table>
             <tr>
-            <th>ID</th>
-            <th>Nom</th>
-            <th>Prenom</th>
-            <th>Genre</th>
-            <th>Email</th>
-            <th>Bac</th>
+                <th>ID</th>
+                <th>Nom</th>
+                <th>Prenom</th>
+                <th>Genre</th>
+                <th>Email</th>
+                <th>Bac</th>
+                <th></th>
             </tr>
+            <?php while ($row = mysql_fetch_array($res)) {?>
+            <tr>
+                <td><?php echo $row['id'] ?></td>
+                <td><?php echo $row['nom'] ?></td>
+                <td><?php echo $row['prenom'] ?></td>
+                <td><?php echo $row['genre'] ?></td>
+                <td><?php echo $row['email'] ?></td>
+                <td><?php echo $row['bac'] ?></td>
+                <td>
+                    <form method="POST" action="">
+                        <input type="hidden" name="id" value="<?php echo $row['id'] ?>"/>
+                        <input type="submit" name="delete" value="Delete"/>
 
+                    </form>
+                </td>
+            </tr>
+            <?php } ?>
 
         </table>
     </body>
