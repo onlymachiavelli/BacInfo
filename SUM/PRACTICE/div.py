9d13651e03d82de08ac701b011441048a9076301
onlymachiavelli <tmakaveli643@gmail.com>
2022-04-14 22:05:09 +0100

    stffs

---
+++ SUM/PRACTICE/div.py
@@ -0,0 +1,23 @@
+
+
+def divTwo(n: int):
+    return int(str(n)[len(str(n))-1]) % 2 == 0
+
+
+def det(n: int):
+    chaine = str(n)
+    longeur = len(chaine)
+    lastIndex = int(chaine[longeur-1])
+    return lastIndex % 2 == 0
+
+
+def divThree(n: int):
+    myString = str(n)
+    summ = 0
+    for i in range(len(myString)):
+        summ += int(myString[i])
+    return summ % 3 == 0
+
+
+def divFour(n: int):
+    return int(str(n)[len(str(n))-2: len(str(n))]) % 4 == 0
