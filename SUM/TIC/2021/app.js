915ac35b8c01fa4fa9baf4b4d70c651d84f39735
onlymachiavelli <tmakaveli643@gmail.com>
2022-04-25 15:38:48 +0100

    form control

---
+++ SUM/TIC/2021/app.js
@@ -0,0 +1,37 @@
+function Id(id) {
+  return document.getElementById(id);
+}
+
+function Name(namme) {
+  return document.getElementsByName(namme);
+}
+
+function Test1() {
+  var err = "";
+
+  if (Name("station")[0].selectedIndex == 0) {
+    err += "selectioner la station \n";
+  }
+  if (Name("saison")[0].selectedIndex) {
+    err += "Selectioner saison \n";
+  }
+  if (!Name("year")[0].checked && !Name("year")[1].checked) {
+    err += "choisir l'annee \n";
+  }
+  if (
+    Number(Name("temp")[0].value) < -5 ||
+    Number(Name("temp")[0].value) > 50 ||
+    Name("temp")[0].value == ""
+  ) {
+    err += "temperature invalide \n";
+  }
+  if (Number(Name("pluie")[0].value) < 0 || Name("pluie")[0].value == "") {
+    err += "pluie option invalide";
+  }
+
+  if (err.length > 0) {
+    alert(err);
+    return false;
+  }
+  return true;
+}
