eec3d79b66dd55a3a3fea561d82a7cc529f8c36f
onlymachiavelli <tmakaveli643@gmail.com>
2022-05-05 13:23:42 +0100

    state

---
+++ SUM/TIC/bac2016/etat.php
@@ -0,0 +1,10 @@
+<?php
+mysql_connect("localhost" , "root" , "") or die("Error connecting to the server") ;
+mysql_select_db("bac2016") or die("Error connecting to the db") ;
+
+
+$req = "SELECT ";
+
+
+?>
+
