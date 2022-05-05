60a084bdfd3d724709a91499d631eb1c11fb1d4c
onlymachiavelli <tmakaveli643@gmail.com>
2022-05-05 13:08:54 +0100

    saving datas code fixing

--- SUM/TIC/bac2016/add.php
+++ SUM/TIC/bac2016/add.php
@@ -1,8 +1,8 @@
 <?php
 
 //connect  to the SERVER 
-mysql_connect("localhost" , "root" , "") ;
-mysql_select_db("bac2016") ;
+mysql_connect("localhost" , "root" , "") or die("Error connecting to the server") ;
+mysql_select_db("bac2016") or die("Error connecting to the db") ;
 
 $date = $_POST['year']."-".$_POST['month']."-".$_POST['day'] ;
 
@@ -11,18 +11,17 @@ $salle = $_POST['salle'] ;
 $checkSalle = "SELECT Idsalle from spectacle WHERE Idsalle=$salle AND DateSpectacle=$date";
 $sl = mysql_query($checkSalle);
 
+
 if(!$sl) {
     //ekhdem ! 
+
     $checkPeace = "SELECT Idpiece FROM spectacle WHERE Idpiece=$peace";
     $pc = mysql_query($checkPeace) ;
-    if (mysql_num_rows($pc) > 0 ) {
-        //err
-        echo "Piece deja programmee" ; 
-    }
-    else {
-        //ekhdem
-        $req = "INSERT INTO spectacle VALUES($peace, $date, $salle)";
+    if (mysql_num_rows($pc) == 0) {
+        
+        $req = "INSERT INTO spectacle VALUES('$peace', '$date', '$salle')";
         $res = mysql_query($req) ;
+        
         if ($res) {
             echo "done saving the datas" ;
         }
@@ -30,10 +29,16 @@ if(!$sl) {
             echo "error saving the datas ! <br/> ".mysql_error() ;
         }
     }
+
+
+
+    else {
+        echo "Piece deja programmee" ; 
+    }
+}
 else{
     //err
     echo "salle non disponible" ; 
-}
 }
 
 ?>
