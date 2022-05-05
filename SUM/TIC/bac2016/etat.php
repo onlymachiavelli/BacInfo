6b37a5e403f147bd5a99b80094a56318918b7b99
onlymachiavelli <tmakaveli643@gmail.com>
2022-05-05 13:39:02 +0100

    joining tables

--- SUM/TIC/bac2016/etat.php
+++ SUM/TIC/bac2016/etat.php
@@ -3,7 +3,8 @@ mysql_connect("localhost" , "root" , "") or die("Error connecting to the server"
 mysql_select_db("bac2016") or die("Error connecting to the db") ;
 
 
-$req = "SELECT ";
+$req = "SELECT p.Titre, s.libelle , sp.DateSpectacle , sp.Idpiece ,sp.Idsalle , s.Idpiece, p.Idpiece, s.Idsalle FROM piece AS p, salle AS  s, spectacle AS  sp WHERE sp.Idpiece = p.Idpiece AND sp.Idsalle = s.Idsalle";
+$res = mysql_query($req);
 
 
 ?>
