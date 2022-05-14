32d0f12a3a9c3524271aa13af3bb5b6dc34a990b
onlymachiavelli <tmakaveli643@gmail.com>
2022-05-14 08:42:44 +0100

    strc

--- SUM/PRACTICE/C/pra19.cpp
+++ SUM/PRACTICE/C/pra19.cpp
@@ -8,19 +8,27 @@ struct thePpcm{
 
 int size() {
     int n;
+    cout << "Enter Size"  << endl;
     cin >> n;
     if (n<2 || n>100){
         return size();
     }
     return n;
 }
+int num(){
+    return 0;
+}
 void fillFile(FILE * myFile, int n ){
-    
+    thePpcm obj;
+    for (int i=0;i<n;i++){
+        obj.a = num();
+        obj.b = num();
+    }   
 }
 int main (){
     FILE *myFile = fopen("ppcm.bin", "wb");
     
-
+    int n = size();
     fclose(myFile);
     
 }
