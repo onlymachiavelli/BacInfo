49329079738b73489daf52dd3cfa11b589fa71ea
onlymachiavelli <tmakaveli643@gmail.com>
2022-05-17 21:09:48 +0100

    boo

--- TIC/PiggieTalk/Backend/src/connect.php
+++ TIC/PiggieTalk/Backend/src/connect.php
@@ -6,7 +6,8 @@ function connectDB () {
     mysql_select_db("piggietalk") or die(mysql_error());
 }
 
-function sendReq ($request) {
-    mysql_query($request) or die(mysql_error());
-}
+function sendReq ($request, $get) {
+    $res = mysql_query($request) or die(mysql_error());
+    return $get ? mysql_affected_rows($res) : mysql_num_rows($res);
+}     
 ?>
