c383230405a268036a572819caa3df6e2de6e7bb
onlymachiavelli <tmakaveli643@gmail.com>
2022-05-18 19:09:35 +0100

    stffs

--- SUM/prototype/p1/lister.php
+++ SUM/prototype/p1/lister.php
@@ -6,8 +6,44 @@ mysql_select_db("location") or die(mysql_error()."<br/>") ;
 
 
 
-$req = "select * from voiture where disponible =N";
+$req = "select * from voiture where Disponible='N'";
 $res = mysql_query($req);
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <?php
+    
+        if (mysql_num_rows($res) ==0) {
+            echo "<h1>No datas ! </h1>" ;
+        }
+        else {
+            while ($row=mysql_fetch_row($res)){
+                ?>
+            <table>
+                <tr>
+                <th>Matricule Voiture</th>
+                <th>Modele  Voiture</th>
+                <th>Prix de location </th>
+                </tr>
+
+                <tr>
+                <td><?php echo $row["Imat"] ;?></td>
+                <td><?php echo $row["Model"] ;?></td>
+                <td><?php echo $row["PrixLoc"] ;?></td>
+                </tr>
+            </table>
+
+        <?php }}?>
+    ?>
+    
+</body>
+</html>
