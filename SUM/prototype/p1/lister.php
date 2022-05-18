2bf7fdcdc9cc01a8e2178259903674583c6615bb
onlymachiavelli <tmakaveli643@gmail.com>
2022-05-18 18:53:55 +0100

    js fixing stffs

--- SUM/prototype/p1/lister.php
+++ SUM/prototype/p1/lister.php
@@ -1,4 +1,13 @@
 <?php
 
+//connect !  
+mysql_connect("localhost", "root" , "") or die(mysql_error()."<br/>") ;
+mysql_select_db("location") or die(mysql_error()."<br/>") ;
+
+
+
+$req = "select * from voiture where disponible =N";
+$res = mysql_query($req);
+
 
 ?>
