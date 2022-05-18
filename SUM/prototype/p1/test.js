bd15f661d9a567d3298d7f4497276bfd1ad7adb3
onlymachiavelli <tmakaveli643@gmail.com>
2022-05-18 18:16:43 +0100

    done js test1

---
+++ SUM/prototype/p1/test.js
@@ -0,0 +1,62 @@
+function Id(id) {
+  return document.getElementById(id);
+}
+
+function Name(namme) {
+  return document.getElementsByName(namme);
+}
+
+function isAlpha(myString) {
+  for (var i = 0; i < myString.length; i++) {
+    if (
+      myString.charAt(i).toLowerCase() < "a" ||
+      myString.charAt(i).toLowerCase() > "z"
+    ) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function isNumeric(myString) {
+  for (var i = 0; i < myString.length; i++) {
+    if (
+      myString.charAt(i).toLowerCase() < "0" ||
+      myString.charAt(i).toLowerCase() > "9"
+    ) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function test1() {
+  var err = "";
+  if (Id("ncin").value.length != 8) {
+    err += "NCIN field is invalid ! \n";
+  }
+  if (Id("nom").value.length < 3 || !isAlpha(Id("nom").value)) {
+    err += "invalid name \n";
+  }
+
+  if (Id("prenom").value.length < 3 || !isAlpha(Id("prenom").value)) {
+    err += "invalid last name \n";
+  }
+
+  var allowedNumbers = new Array(0, 1, 6);
+  var phone = true;
+  for (var i = 0; i < 3; i++) {
+    if (allowedNumbers[i] === Number(Id("tel").value.charAt(0))) {
+      phone = false;
+    }
+  }
+  if (!phone || Id("tel").length === 0) {
+    err += "Invalid phone ! \n";
+  }
+
+  if (err.length > 0) {
+    alert(err);
+    return false;
+  }
+  return true;
+}
