c2b6c1e00069800921d7b781afc52d1ff9b9ed7b
Mahciavelli <tmakaveli643@gmail.com>
2022-05-19 23:10:31 +0100

    pi value

--- ALGO/euler.py
+++ ALGO/euler.py
@@ -1,3 +1,5 @@
+from math import *
+
 def fact(n: int) -> int:
     if n == 0:
         return 1
@@ -5,7 +7,7 @@ def fact(n: int) -> int:
 
 
 
-def euler() -> int:
+def euler() -> float:
     n=0
     s1 = 1/fact(n)
     n+=1
@@ -16,4 +18,32 @@ def euler() -> int:
         s2 = s1 + 1/fact(n)
     return s2
 
-print(euler())
+def power (x: float, n: int) -> float:
+    if n == 0:
+        return 1
+    return x * power(x, n-1)
+
+
+def eulerPIusingFact(ep:float) -> float :
+    n=1 
+    s1 = (1/fact(n))
+    n+=1 
+    s2 = s1 + (1/fact(n))
+    while abs(s1*6 - s2*6) >= ep:
+        s1 = s2
+        n+=1
+        s2 = s1 + (1/fact(n))
+    return sqrt(s2*6)
+def eulerPI(ep:float) -> float  :
+    n=1 
+    s1 = 1/power(n, 2) 
+    n+=1 
+    s2 =s1 + 1/power(n, 2)
+    while abs(s2-s1) >= ep :
+        s1 = s2
+        n+=1
+        s2 = s1 + 1/power(n,2)
+    return sqrt(s2*6)
+
+
+print(eulerPI(0.00000001))
