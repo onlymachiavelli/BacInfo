4608f6d4c0aeae7bce642ccebdd5999b2c6f4f69
Mahciavelli <tmakaveli643@gmail.com>
2022-05-19 21:54:45 +0100

    sinus done

--- ALGO/apprLearning.py
+++ ALGO/apprLearning.py
@@ -14,15 +14,16 @@ def sinus(x:float, ep:float) -> float :
     one = 1 
     n = 1 
     s1 = power(x,1) / fact(1) * one 
+    
     one *= -1 
     n+=2 
-    s2 = s1 + power(x,n) / fact(n) * one 
+    s2 = s1 + (power(x,n) / fact(n)) * one 
     one *= -1
     while abs(s1 - s2) >= ep:
         s1 = s2
         n+=2
-        s2 = power(x,n) / fact(n)
+        s2 = s1 + (power(x,n) / fact(n))*one
         one *= -1
     return s2
 
-print(sinus(3, 0.001))
+print(sinus(1, 0.0000000001))
