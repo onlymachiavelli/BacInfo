f66d1b5d7547fcf3992fc4c2df071f39bdcd536c
Mahciavelli <tmakaveli643@gmail.com>
2022-05-21 09:51:15 +0100

    done js

--- TIC/2018/controle.js
+++ TIC/2018/controle.js
@@ -25,16 +25,60 @@ function genCaptcha() {
 
 function verifyCap(cap, res) {
   var capitals = 0;
-  for (var i = 0; i < cap.length; i++) {
+  for (var i = 0; i <= cap.length; i++) {
     if (cap[i] >= "A" && cap[i] <= "Z") {
       capitals++;
     }
   }
-  return res === capitals;
+  alert(capitals);
+  return Number(res) === capitals;
 }
 
 function setCap() {
   Id("captcha").value = genCaptcha();
+}
+
+function numeric(ch) {
+  for (var i = 0; i < ch.length; i++) {
+    if (ch[i] < "0" || ch[i] > "9") {
+      return false;
+    }
+  }
+  return true;
+}
+function verif() {
+  var err = "";
+  if (Id("hotel").selectedIndex === 0) {
+    err += "you should select a hotel \n ";
+  }
+  if (
+    !Name("acc")[0].checked &&
+    !Name("acc")[1].checked &&
+    !Name("acc")[2].checked
+  ) {
+    err += "You should select a welcome type \n";
+  }
+
+  if (
+    !Name("res")[0].checked &&
+    !Name("res")[1].checked &&
+    !Name("res")[2].checked
+  ) {
+    err += "You should select a reservation type \n";
+  }
+  if (numeric(Id("rep").value)) {
+    if (!verifyCap(Id("captcha").value, Id("rep").value)) {
+      err += "You should enter the correct number of capital letters \n";
+    }
+  } else {
+    err += "You should enter a number \n";
+  }
+
+  if (err.length > 0) {
+    alert(err);
+    return false;
+  }
+  return true;
 }
 
 setCap();
