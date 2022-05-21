1ad752b2cf59ee0222eef2f301e71753f969780f
Mahciavelli <tmakaveli643@gmail.com>
2022-05-21 09:21:02 +0100

    captcha shits

--- TIC/2018/controle.js
+++ TIC/2018/controle.js
@@ -5,3 +5,30 @@ function Id(id) {
 function Name(namme) {
   return document.getElementsByName(namme);
 }
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+function genCaptcha() {
+  var cap = "";
+  var k;
+  for (var i = 0; i < 10; i++) {
+    k = random(0, 26);
+    if (k % 2 == 0) {
+      cap += String.fromCharCode(k + 65);
+    } else {
+      cap += String.fromCharCode(k + 97);
+    }
+  }
+  return cap;
+}
+
+function verifyCap(cap, res) {
+  return true;
+}
+
+function setCap() {
+  Id("captcha").value = genCaptcha();
+}
+
+setCap();
