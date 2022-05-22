6d6390d175aa8fc54c58c9783f7aa75da06badc3
Mahciavelli <tmakaveli643@gmail.com>
2022-05-22 14:31:36 +0100

    exam 2020'

--- TIC/2020/biblio.js
+++ TIC/2020/biblio.js
@@ -5,3 +5,22 @@ function Id(id) {
 function Name(namme) {
   return document.getElementsByName(namme);
 }
+
+function isAlpha(ch) {
+  for (var i = 0; i < ch.length; i++) {
+    if (ch[i].toLowerCase() < "a" || ch[i].toLowerCase() > "z") {
+      return false;
+    }
+  }
+
+  return true;
+}
+function verif1() {
+  var err = "";
+  if (Id("nom").value.length < 3 || !isAlpha(Id("nom").value)) {
+    if (err.length > 0) {
+      alert(err);
+      return false;
+    }
+  }
+}
