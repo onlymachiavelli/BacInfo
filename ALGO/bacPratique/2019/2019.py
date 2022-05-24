e9bdef40e662aa0238ff88ed68cfaeeb8ea908f5
onlymachiavelli <tmakaveli643@gmail.com>
2022-05-24 19:00:36 +0100

    stffs

--- ALGO/bacPratique/2019/2019.py
+++ ALGO/bacPratique/2019/2019.py
@@ -1,31 +1,40 @@
 
+import numpy as np 
 
-
-def size():
+def interval( mi, ma) :
     n = int(input("Enter n ")) 
-    if  2 <= n <= 100 :
+    if  mi <= n <= ma :
         return n 
-    return size()
+    return interval(mi, ma) 
 
 
 
-def ppcm(a,b):
-    datas = {
-        "a" : "",
-        "b" : ""
-    }
+def isPrime(n, i=2):
+    if n <2 :
+        return False
+    if i <= n//2 :
+        return n % i != 0 and isPrime(n, i+1) 
+    return True
 
-    n = 2
-    na = a
-    while na % n != 0:
-        print(na)
-        if na % n == 0:
-            datas["a"] += str(n)
-            na //=2
+
+
+
+def nums(num, res, n):
+    a = num 
+    prime  = 2 
+    while prime  <= a :
+        if a % prime != 0 :
+            prime +=1 
+            while not isPrime(prime) :
+                prime +=1 
         else :
+            res[n] = prime
+            a //= prime 
             n +=1 
-            datas["a"] += "/"
-    return datas 
 
+def ppcm(p, q) :
+    arr = np.array([int] *a)
+    myFile = open("result.dat", "wb") 
+    return 0
 
-print(ppcm(168,24))
+print(nums(35))
