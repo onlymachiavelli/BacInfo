98b9fb3d76105cb056097111287c2e895b1f683b
onlymachiavelli <tmakaveli643@gmail.com>
2022-05-25 13:57:16 +0100

    fck

--- ALGO/bacPratique/2019/2019_1.py
+++ ALGO/bacPratique/2019/2019_1.py
@@ -4,6 +4,7 @@ def size(mi, ma) :
     n = int(input()) 
     if mi <= n <= ma :
         return n 
+    print("Enter it again ! ")
     return size(mi, ma )
 
 
@@ -28,7 +29,7 @@ def getPrime(primi,arr1,l1,  n) :
     number = n
     count = 0
     l = 0
-    while number > prime :
+    while number >= prime :
         if number % prime != 0 :
             l+=1 
             prime = primi[l]
@@ -48,7 +49,6 @@ def ppcm(p, q) :
     thePrimes = np.array([int] * 100)# array of prime numbers 
     primeLen = 0
     fillPrimeNumbers(thePrimes,primeLen, (p+q)//2 )
-
     arr1 = np.array([int] * 100 ) # array of the numbers of p 
     arr2 = np.array([int] * 100 )# array of the numbers of q
     len1= 0
@@ -56,22 +56,53 @@ def ppcm(p, q) :
     getPrime(thePrimes, arr1 , len1, p)
     getPrime(thePrimes, arr2 , len2, q)
     res =1 
-    while len1 != len2 :
-        big = arr1 
-        small = arr2 
-        b = len1 
-        s = len2 
-        if len1 < len2 :
-            big, small = small, big 
-            s, b = b,s 
-        while b != s :
-            s+=1 
-            small[s] = 0
+    big = arr1 
+    small = arr2 
+    b = len1 
+    s = len2
+    if len1 < len2 :
 
-        for i in range (b):
-            if big[i] > 0 and small[i] > 0 :
-                if big[i] > small[i] :
-                    res *= power(thePrimes[i], big[i])
-            else :
-                res *= power(thePrimes[i], small[i])
+        big = arr2 
+        small = arr1
+        b = len2
+        s = len1
+    print (s< b)
+    while b != s :
+        s+=1 
+        small[s] = 0
 
+    for i in range (b):
+        if big[i] > 0 and small[i] > 0 :
+            if big[i] > small[i] :
+                res *= power(thePrimes[i], big[i])
+        else :
+            res *= power(thePrimes[i], small[i])
+    return res 
+
+def saveFile(n) :
+    myfile = open("result.dat", "wb")
+    for i in range (n) :
+        print("Give  p ") 
+        p = size(1, 1000)
+        print("Give q ")
+        q = size(1 , 1000)
+        pk.dump({
+            "a" : p, 
+            "b" : q ,
+            "ppcm" : ppcm(p,q) 
+        }, myfile)
+
+    myfile.close()
+
+def Show(n):
+    myFile=  open("result.dat" , "rb")  
+    for i in range (n):
+        obj = pk.load(myFile)
+        print("PPCM(", obj["a"] , ",",obj["b"] , ") = " , obj["ppcm"])
+    myFile.close()
+
+
+print("Enter n ! ")
+n = size(2 , 100)
+saveFile(n)
+Show(n)
