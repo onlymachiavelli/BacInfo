a21b5927ac8a3aa55b2eaf77f5b4fbde249c05b8
Mahciavelli <tmakaveli643@gmail.com>
2022-05-27 20:56:58 +0100

    stffs

--- ALGO/bacPratique/2020/bac2020.py
+++ ALGO/bacPratique/2020/bac2020.py
@@ -1,27 +1,5 @@
 import numpy as np 
-
 import pickle as pk 
 
-def getN(code):
-    if code.isdecimal() :
-        return int(code)
-
-    return ord(code) - 55
-
-def power(a,b) :
-    if b > 1 :
-        return a * power(a, b-1)
-    return a
-
-def toDec(code, base) :
-    res = 0 
-    for i in range(len(code)) :
-        res += power(base, i) * getN(code[len(code) -1-i])
-        print(getN(code[len(code) -1-i]))
-
-    return res
-
-
-print(toDec("A", 16))
 
 
