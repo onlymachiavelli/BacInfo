00745205224e313bf1e8dc02f36743f39ab03cb9
onlymachiavelli <tmakaveli643@gmail.com>
2022-05-28 14:33:45 +0100

    php stffs

--- MACHIAVELLI PRINCIPAL/STI/enregistrement.php
+++ MACHIAVELLI PRINCIPAL/STI/enregistrement.php
@@ -27,4 +27,5 @@ else {
     }
 }
 
+mysql_close();
 ?>
--- MACHIAVELLI PRINCIPAL/STI/evaluation.php
+++ MACHIAVELLI PRINCIPAL/STI/evaluation.php
@@ -13,12 +13,30 @@ $cond = $_POST["cond"] ;
 $conf = $_POST["conf"] ;
 
 if (mysql_num_rows($check) > 0 ) {
-    $check2= "SELECT * FROM evaluation WHERE numPermis = '$id' AND "
+    $check2= "SELECT * FROM evaluation WHERE numPermis = '$id' AND idModele=$model" ;
+    $checkres = mysql_query($check2);
+    if (mysql_num_rows($checkres) >0) {
+        die ("already rated ! ") ;
+    }
+    else {
+        $date = date("Y-m-d h:i:s") ;
+        $req = "INSERT INTO evaluation VALUES('$id', $model,'$date', $sec, $cond, $conf)";
+        $res = mysql_query($req) ;
+        if ($res){
+            print("Done saving the datas ! ") ;
+
+        }
+        else {
+            die (mysql_error() );
+        }
+    }
 }
 else{
     die("USER is not found ! ") ;
 
 }
+
+mysql_close();
 
 
 ?>
